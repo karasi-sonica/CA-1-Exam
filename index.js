da4ebfcb617f7e858d49b226d120e94175b481c5
karasi-sonica <karasisonica@gmail.com>
2025-01-27 09:25:45 +0530

    commited

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express'); 
+const app = require('index.html');
+
+if (Username == null) {
+    console.log("Username cannot be empty");
+} else {
+    console.log("Username is valid!!")
+}
+
+if (E-mail == null) {
+    console.log("Email cannot be empty")
+}
